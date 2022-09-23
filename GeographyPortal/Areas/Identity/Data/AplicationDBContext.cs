89b5ec1df318a4704bd5bb6b5914469f2cf8c8d7
Valentine <valmak29@gmail.com>
2022-09-23 14:44:21 +0400

    Make first auth

---
+++ GeographyPortal/Areas/Identity/Data/AplicationDBContext.cs
@@ -0,0 +1,36 @@
+﻿using GeographyPortal.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GeographyPortal.Areas.Identity.Data;
+
+public class AplicationDBContext : IdentityDbContext<GeographyPortalUser>
+{
+    public AplicationDBContext(DbContextOptions<AplicationDBContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+}
+
+
+public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<GeographyPortalUser>
+{
+    public void Configure(EntityTypeBuilder<GeographyPortalUser> builder)
+    {
+        builder.Property(o => o.FirstName).HasMaxLength(255);
+        builder.Property(o => o.LastName).HasMaxLength(255);
+
+    }
+}
--- GeographyPortal/Program.cs
+++ GeographyPortal/Program.cs
@@ -1,6 +1,16 @@
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using GeographyPortal.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AplicationDBContextConnection") ?? throw new InvalidOperationException("Connection string 'AplicationDBContextConnection' not found.");
+
+builder.Services.AddDbContext<AplicationDBContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<GeographyPortalUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AplicationDBContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -20,6 +30,7 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
@@ -33,5 +44,7 @@ app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("v1/swagger.json", "My API V1");
 });
+
+app.MapRazorPages();
 
 app.Run();
