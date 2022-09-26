c22858acb1bf8ce78cd1918a858f94e709bd4d31
Danilos-Prog <daniilmutaliev2001@gmail.com>
2022-09-26 16:35:07 +0400

    feat(Test.cs): add comments

--- GeographyPortal/Models/Test.cs
+++ GeographyPortal/Models/Test.cs
@@ -4,6 +4,9 @@ using MongoDB.Bson.Serialization.IdGenerators;
 
 namespace GeographyPortal.Models
 {
+    /// <summary>
+    /// Test Entity
+    /// </summary>
     public class Test
     {
         [BsonId(IdGenerator = typeof(GuidGenerator))]
