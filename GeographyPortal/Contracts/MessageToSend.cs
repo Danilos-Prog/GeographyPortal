4e8777ce519dd70b97b545970b49031223ed3318
“KruglovIlya” <kruglov-il1@yandex.ru>
2022-09-27 01:41:28 +0400

    add record MessageToSend

---
+++ GeographyPortal/Contracts/MessageToSend.cs
@@ -0,0 +1,13 @@
+﻿namespace GeographyPortal.Container.Messages
+{
+    public record MessageToSend
+    {
+        public string EmailAdress { get; init; }
+
+        public string TextOfEmail { get; init; }
+
+        public string Subject { get; init; }
+
+
+    }
+}
