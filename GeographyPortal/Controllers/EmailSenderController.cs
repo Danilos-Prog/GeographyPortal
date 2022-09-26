98fcb696c5163bb74e2f2c5056f66b948c765b95
“KruglovIlya” <kruglov-il1@yandex.ru>
2022-09-27 01:42:23 +0400

    rewrite EmailSenderController

---
+++ GeographyPortal/Controllers/EmailSenderController.cs
@@ -0,0 +1,28 @@
+﻿using GeographyPortal.Services;
+using GeographyPortal.Services.Impl;
+using GeographyPortal.Services.Publishers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeographyPortal.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmailSenderController : Controller
+    {
+        private readonly IEmailSenderService _emailSenderService;
+
+        public EmailSenderController(IEmailSenderService emailSenderService)
+        {
+            _emailSenderService = emailSenderService;
+        }
+
+
+        [HttpPost]
+        public void SendEmail(string email, string title, string subject)
+        {
+            
+        }
+
+    }
+}
